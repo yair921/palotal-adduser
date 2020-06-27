6ae61015f98b2d20c72382b06a2f13033450d80b
Yair Montes <yair_918@hotmail.com>
2020-06-27 12:57:31 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+
+
+
+exports.handler = async function (event, context) {
+    // console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+    // return context.logStreamName
+
+    // Here your code....
+}
